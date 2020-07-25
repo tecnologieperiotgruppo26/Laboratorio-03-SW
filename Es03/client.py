f5357c50793036b2856555eb235b4b5ce22756c1
Simone <simone.alberto@icloud.com>
2020-07-25 17:56:55 +0200

    fix

--- Es03/client.py
+++ Es03/client.py
@@ -147,31 +147,10 @@ if __name__ == '__main__':
     input_val = int(input("Enter command number: "))
 
     if input_val == 1:
-      client.mySecondPublish(topicMessage, setJSONCommand(input_val))
+      client.mySecondPublish(topic, setJSONCommand(input_val))
     elif input_val == 2:
-      client.mySecondPublish(topicMessage, setJSONCommand(input_val))
+      client.mySecondPublish(topic, setJSONCommand(input_val))
     elif input_val == 0:
       break
-
-  print("Available options: ")
-  print("0 - Insert subtopic")
-  print("1 - Insert device value")
-  print("2 - Exit")
-  input_val = int(input("Enter command number: "))
-  if input_val == 0:
-    subTopic = input("Insert device subtopic: ")
-    topicMessage = topic + subTopic
-    client.mySubscribe(f"{topicMessage}")
-    value = int(input("Insert device value: "))
-    client.mySecondPublish(topicMessage, value)
-  elif input_val == 1:
-    if (topicMessage == ""):
-      print("You have to insert the topic!")
-      pass
-    else:
-      value = int(input("Insert device value: "))
-      client.mySecondPublish(topicMessage, value)
-  elif input_val == 2:
-    break
 
   client.end()
